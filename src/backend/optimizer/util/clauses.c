eb8b98723b788d63e4ec44cc355c0055d5bc785a
Ekta Khanna <ekhanna@pivotal.io>
2017-05-09 11:12:28 -0700

    CDBlize the cherry-pick
    Original Flow:
    cdb_flatten_sublinks
    	+--> pull_up_IN_clauses
    		+--> convert_sublink_to_join
    
    New Flow:
    cdb_flatten_sublinks
    	+--> pull_up_sublinks
    
    This commit contains relevant changes for the above flow.
    
    Previously, `try_join_unique` was part of `InClauseInfo`. It was getting
    set in `convert_IN_to_join()` and used in `cdb_make_rel_dedup_info()`.
    Now, since `InClauseInfo` is not present and we construct
    `FlattenedSublink` instead in `convert_ANY_sublink_to_join()`. And later
    in the flow, we construct `SpecialJoinInfo` from `FlattenedSublink` in
    `deconstruct_sublink_quals_to_rel()`. Hence, adding `try_join_unique` as
    part of both `FlattenedSublink` and `SpecialJoinInfo`.
    
    Ref [#142355175]
    
    Signed-off-by: Ekta Khanna <ekhanna@pivotal.io>

--- src/backend/optimizer/util/clauses.c
+++ src/backend/optimizer/util/clauses.c
@@ -3200,6 +3200,7 @@ eval_const_expressions_mutator(Node *node,
 		newfslink->lefthand = fslink->lefthand;
 		newfslink->righthand = fslink->righthand;
 		newfslink->quals = quals;
+		newfslink->try_join_unique = fslink->try_join_unique;
 		return (Node *) newfslink;
 	}
 
