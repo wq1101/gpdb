a14b9db73f7d4743a5577e0eb421f648fda5e9ce
Ekta Khanna <ekhanna@pivotal.io>
2017-05-10 18:47:20 -0700

    Replace JOIN_LASJ by JOIN_ANTI
    After this cherry-pick, Anti Semi Join will
    be denoted by `JOIN_ANTI` instead of `JOIN_LASJ`
    
    Ref [#142355175]
    
    Signed-off-by: Dhanashree Kashid <dkashid@pivotal.io>

--- src/backend/commands/explain.c
+++ src/backend/commands/explain.c
@@ -966,9 +966,6 @@ explain_outNode(StringInfo str,
 				case JOIN_ANTI:
 					pname = "Nested Loop Anti Join";
 					break;
-				case JOIN_LASJ:
-					pname = "Nested Loop Left Anti Semi Join";
-					break;
 				case JOIN_LASJ_NOTIN:
 					pname = "Nested Loop Left Anti Semi Join (Not-In)";
 					break;
@@ -998,9 +995,6 @@ explain_outNode(StringInfo str,
 				case JOIN_ANTI:
 					pname = "Merge Anti Join";
 					break;
-				case JOIN_LASJ:
-					pname = "Merge Left Anti Semi Join";
-					break;
 				case JOIN_LASJ_NOTIN:
 					pname = "Merge Left Anti Semi Join (Not-In)";
 					break;
@@ -1029,9 +1023,6 @@ explain_outNode(StringInfo str,
 					break;
 				case JOIN_ANTI:
 					pname = "Hash Anti Join";
-					break;
-				case JOIN_LASJ:
-					pname = "Hash Left Anti Semi Join";
 					break;
 				case JOIN_LASJ_NOTIN:
 					pname = "Hash Left Anti Semi Join (Not-In)";
